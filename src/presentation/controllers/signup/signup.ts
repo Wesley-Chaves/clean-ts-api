3cab0ef26f628ba5839b632b0628186994cfe388
Wesley Spectre <wesleychavesdev@gmail.com>
2023-11-16 10:27:06 -0300

    feat: create EmailValidator interface

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -1,12 +1,13 @@
 import { Controller } from '../../protocols/controller'
+import { EmailValidator } from '../../protocols/emailValidator'
 import { HttpRequest, HttpResponse } from '../../protocols/http'
 import { MissingParamError } from '../errors/missing-param-error'
 import { badRequest } from '../helpers/http-helper'
 
 export class SignUpController implements Controller {
-  private readonly emailValidator: any
+  private readonly emailValidator: EmailValidator
 
-  constructor (emailValidator: any) {
+  constructor (emailValidator: EmailValidator) {
     this.emailValidator = emailValidator
   }
 
