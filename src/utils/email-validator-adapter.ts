751a1d3f54ec52fb681d315a3e3a864313bf08fc
Wesley Spectre <wesleychavesdev@gmail.com>
2023-11-16 16:26:35 -0300

    feat: ensure EmailValidatorAdapter returns false if validator returns false

---
+++ src/utils/email-validator-adapter.ts
@@ -0,0 +1,8 @@
+import { EmailValidator } from '../presentation/protocols/emailValidator'
+import validator from 'validator'
+
+export class EmailValidatorAdapter implements EmailValidator {
+  async isValid (email: string): Promise<boolean> {
+    return await new Promise((resolve) => { resolve(validator.isEmail(email)) })
+  }
+}
