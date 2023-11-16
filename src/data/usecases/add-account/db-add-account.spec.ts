57a83d0127f92a9a05b007952652fc36966fb728
Wesley Spectre <wesleychavesdev@gmail.com>
2023-11-16 18:32:50 -0300

    test: ensure DbAddAccount throws if AddAccountRepository throws

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -71,4 +71,11 @@ describe('DbAddAccount Usecase', () => {
     await sut.add(accountData)
     expect(addSpy).toHaveBeenCalledWith(Object.assign({}, accountData, { password: 'hashed_password' }))
   })
+
+  test('Should throw if AddAccountRepository throws', async () => {
+    const { sut, addAccountRepositoryStub } = makeSut()
+    jest.spyOn(addAccountRepositoryStub, 'add').mockReturnValueOnce(new Promise((resolve, reject) => { reject(new Error()) }))
+    const promise = sut.add(accountData)
+    await expect(promise).rejects.toThrow()
+  })
 })
