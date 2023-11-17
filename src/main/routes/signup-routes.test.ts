d19329cfe0062039b5fafc428dddd338710a52df
Wesley Spectre <wesleychavesdev@gmail.com>
2023-11-17 15:23:58 -0300

    refactor: connect to mongodb before integration tests

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -1,5 +1,19 @@
+import { MongoHelper } from '../../infra/database/mongodb/helpers/mongo-helper'
 import app from '../config/app'
 import request from 'supertest'
+
+beforeAll(async () => {
+  await MongoHelper.connect('mongodb://localhost:27017')
+})
+
+beforeEach(async () => {
+  const accountsCollection = await MongoHelper.getCollection('accounts')
+  await accountsCollection.deleteMany({})
+})
+
+afterAll(async () => {
+  await MongoHelper.disconnect()
+})
 
 describe('', () => {
   test('', async () => {
