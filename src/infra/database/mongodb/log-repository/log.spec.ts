6fbbb5d7fef2c0246c4ac4188071add293ab1e92
Wesley Spectre <wesleychavesdev@gmail.com>
2023-11-18 17:03:57 -0300

    test: ensure LogMongoRepository creates an error log on sucess

---
+++ src/infra/database/mongodb/log-repository/log.spec.ts
@@ -0,0 +1,29 @@
+import { MongoHelper } from '../helpers/mongo-helper'
+import { LogMongoRepository } from './log'
+
+const generatedError = new Error()
+generatedError.stack = 'any_stack'
+
+describe('Log Mongo Repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect('mongodb://localhost:27017/clean-node-api')
+  })
+
+  beforeEach(async () => {
+    const errorsCollection = await MongoHelper.getCollection('errors')
+    await errorsCollection.deleteMany({})
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+    MongoHelper.client = null
+  })
+
+  test('Should create an error log on success', async () => {
+    const sut = new LogMongoRepository()
+    await sut.logError(generatedError.stack)
+    const errorsCollection = await MongoHelper.getCollection('errors')
+    const count = await errorsCollection.countDocuments()
+    expect(count).toBe(1)
+  })
+})
--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -26,7 +26,7 @@ generetedError.stack = 'any_stack'
 
 const makeLogErrorRepositoryStub = (): LogErrorRepository => {
   class LogErrorRepositoryStub implements LogErrorRepository {
-    async log (stack: string): Promise<void> {
+    async logError (stack: string): Promise<void> {
       await new Promise((resolve) => { resolve(null) })
     }
   }
@@ -87,7 +87,7 @@ describe('LogController Decorator', () => {
 
   test('Should call LogErrorRepository with correct error if controller returns a server error', async () => {
     const { sut, controllerStub, logErrorRepositoryStub } = makeSut()
-    const logSpy = jest.spyOn(logErrorRepositoryStub, 'log')
+    const logSpy = jest.spyOn(logErrorRepositoryStub, 'logError')
     jest.spyOn(controllerStub, 'handle').mockReturnValueOnce(new Promise((resolve) => { resolve(serverError(generetedError)) }))
     const httpRequest = {
       body: {
