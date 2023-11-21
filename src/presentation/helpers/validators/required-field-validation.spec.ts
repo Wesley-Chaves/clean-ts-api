416ac46e5dd3fbe360ef544af8b799dc49af68a8
Wesley Spectre <wesleychavesdev@gmail.com>
2023-11-21 10:48:10 -0300

    test: ensure RequiredFieldValidation returns a MissingParamError if validation fails

---
+++ src/presentation/helpers/validators/required-field-validation.spec.ts
@@ -0,0 +1,17 @@
+import { MissingParamError } from '../../errors'
+import { RequiredFieldValidation } from './required-field-validation'
+
+describe('RequiredField Validation', () => {
+  test('Should return a MissingParamError if validation fails', async () => {
+    const sut = new RequiredFieldValidation('name')
+    const httpRequest = {
+      body: {
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const error = await sut.validate(httpRequest.body)
+    expect(error).toEqual(new MissingParamError('name'))
+  })
+})
