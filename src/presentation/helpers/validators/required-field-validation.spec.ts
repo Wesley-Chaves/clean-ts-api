ca83658067c3911a9353abdf6471469e87381f3d
Wesley Spectre <wesleychavesdev@gmail.com>
2023-11-21 10:50:29 -0300

    test: ensure RequiredFieldValidation not return if validation pass

--- src/presentation/helpers/validators/required-field-validation.spec.ts
+++ src/presentation/helpers/validators/required-field-validation.spec.ts
@@ -14,4 +14,18 @@ describe('RequiredField Validation', () => {
     const error = await sut.validate(httpRequest.body)
     expect(error).toEqual(new MissingParamError('name'))
   })
+
+  test('Should not return if validation pass', async () => {
+    const sut = new RequiredFieldValidation('name')
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const error = await sut.validate(httpRequest.body)
+    expect(error).toBeFalsy()
+  })
 })
