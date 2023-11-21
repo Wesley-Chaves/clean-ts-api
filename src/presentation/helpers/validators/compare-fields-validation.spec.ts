bd403fa1b08e09be5466ee10c12607bb7bbc26f9
Wesley Spectre <wesleychavesdev@gmail.com>
2023-11-21 11:05:34 -0300

    test: ensure CompareFieldsValdiation not returns if validation pass

--- src/presentation/helpers/validators/compare-fields-validation.spec.ts
+++ src/presentation/helpers/validators/compare-fields-validation.spec.ts
@@ -7,4 +7,10 @@ describe('CompareFields Validation', () => {
     const error = await sut.validate({ password: 'valid_password', passwordConfirmation: 'invalid_confirmation' })
     expect(error).toEqual(new InvalidParamError('passwordConfirmation'))
   })
+
+  test('Should not return if validation pass', async () => {
+    const sut = new CompareFieldsValidation('password', 'passwordConfirmation')
+    const error = await sut.validate({ password: 'valid_password', passwordConfirmation: 'valid_password' })
+    expect(error).toBeFalsy()
+  })
 })
