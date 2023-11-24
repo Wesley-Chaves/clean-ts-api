4b087cc41538863da009c63a787ea9ee239d15a8
Wesley Spectre <wesleychavesdev@gmail.com>
2023-11-24 13:21:23 -0300

    test: ensure DbAuthentication calls LoadAccountByEmailRepository with correct email

---
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -0,0 +1,25 @@
+import { Account } from '../../../domain/models/account'
+import { LoadAccountByEmailRepository } from '../../protocols/load-account-by-email-repository'
+import { DbAuthentication } from './db-authentication'
+
+const fakeAccount: Account = {
+  id: 'any_id',
+  name: 'any_name',
+  email: 'any_email',
+  password: 'any_password'
+}
+
+describe('DbAuthentication Usecase', () => {
+  test('Should call LoudAccountByEmailRepository with correct email', async () => {
+    class LoadAccountByEmailRepositoryStub implements LoadAccountByEmailRepository {
+      async load (email: string): Promise<Account> {
+        return await new Promise((resolve) => { resolve(fakeAccount) })
+      }
+    }
+    const loadAccountByEmailRepositoryStub = new LoadAccountByEmailRepositoryStub()
+    const sut = new DbAuthentication(loadAccountByEmailRepositoryStub)
+    const loadSpy = jest.spyOn(loadAccountByEmailRepositoryStub, 'load')
+    await sut.auth({ email: 'any_email@mail.com', password: 'any_password' })
+    expect(loadSpy).toHaveBeenCalledWith('any_email@mail.com')
+  })
+})
