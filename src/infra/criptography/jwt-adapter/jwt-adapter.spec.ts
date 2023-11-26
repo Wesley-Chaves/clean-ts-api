04c47f75af17076b1a5a37414ba0429dd5ba5814
Wesley Spectre <wesleychavesdev@gmail.com>
2023-11-26 12:02:49 -0300

    test: ensure JwtAdapter throws if jwt sign throws

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -1,11 +1,26 @@
 import jwt from 'jsonwebtoken'
 import { JwtAdapter } from './jwt-adapter'
 
+jest.mock('jsonwebtoken', () => ({
+  sign: async () => {
+    return await new Promise((resolve) => { resolve('accessToken') })
+  }
+}))
+
 describe('Jwt Adapter', () => {
   test('Should call jwt sign with correct values', async () => {
     const sut = new JwtAdapter('secret')
     const signSpy = jest.spyOn(jwt, 'sign')
     await sut.encrypt('any_id')
     expect(signSpy).toHaveBeenCalledWith({ id: 'any_id' }, 'secret')
+  })
+
+  test('Should throw if Jwt throws', async () => {
+    const sut = new JwtAdapter('secret')
+    jest.spyOn(jwt, 'sign').mockImplementationOnce(async () => {
+      await new Promise((resolve, reject) => { reject(new Error()) })
+    })
+    const promise = sut.encrypt('any_id')
+    await expect(promise).rejects.toThrow()
   })
 })
